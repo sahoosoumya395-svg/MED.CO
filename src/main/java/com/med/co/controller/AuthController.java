f41d772026590af335402ca18648f95e03a70f0a
sahoosoumya395-svg <sahoosoumya395@gmail.com>
2026-07-14 21:10:52 +0530

    Updated SecurityConfig and AuthController

--- src/main/java/com/med/co/controller/AuthController.java
+++ src/main/java/com/med/co/controller/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import com.med.co.dto.request.LoginRequest;
 import com.med.co.dto.response.ApiResponse;
 import com.med.co.service.AuthService;
-import com.med.co.service.EmailService;
+
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,23 +15,13 @@ public class AuthController {
     @Autowired
     private AuthService authService;
     
-    @Autowired
-    private EmailService emailService;
+    
 
     @PostMapping("/login")
     public ApiResponse<?> login(@RequestBody LoginRequest request) {
 
         return authService.login(request);
     }
-    @GetMapping("/test-email")
-    public String testEmail() {
-
-        emailService.sendOtpEmail(
-                "subhrajitsamal910@gmail.com",
-                "123456");
-
-        return "Email Sent Successfully";
-    }
-
+   
 
 }
